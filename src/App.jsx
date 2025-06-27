29fbab6742a68f5890873b14c4393cf8f2d5ff9a
parekhmeet <parekhmeet17@gmail.com>
2025-06-27 15:29:45 +0530

    modified base url

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function HomePage() {
 
 function App() {
   return (
-    <Router basename="/moonlights">
+    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/commercial-lights" element={<CommercialLights />} />
